c75083b9336eb2a6bbfde10910b28f14a9d6a91c
GabrielF-22 <gabriel.fernandesl@outlook.com>
2022-08-19 20:10:32 -0300

    Adição dos exercícios 1 e 2

---
+++ M1/B4/Dia-2/exer_2.js
@@ -0,0 +1,7 @@
+let numeros = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+
+for (let index = 0; index < numeros.length; index+=1) {
+  resultado +=numeros[index]
+}
+console.log(resultado)
