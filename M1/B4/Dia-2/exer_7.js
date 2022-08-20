41058b789153b6f1df9149db3c802326ad719971
GabrielF-22 <gabriel.fernandesl@outlook.com>
2022-08-20 19:43:50 -0300

    Terminei o exercício 7

--- M1/B4/Dia-2/exer_7.js
+++ M1/B4/Dia-2/exer_7.js
@@ -1,10 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let menorNumero;
 
+let numeroReferencia = numbers[0]
 for (let index = 0; index < numbers.length; index+=1) {
-    let numeroReferencia = numbers[index]
     if(numbers[index] < numeroReferencia) {
         menorNumero = numbers[index]
     }
 }
-console.log(menorNumero)
+console.log(`O menor número da ARRAY é ${menorNumero}.`)
