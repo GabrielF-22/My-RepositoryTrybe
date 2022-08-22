e26012d42e0e931f5bf070ac8593cc95816f77ce
GabrielF-22 <gabriel.fernandesl@outlook.com>
2022-08-22 20:01:24 -0300

    Adicionei os exerc. 3

---
+++ M1/B4/Dia-3/script_3.js
@@ -0,0 +1,20 @@
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = '';
+
+for (let index = 0; index < array.length; index +=1) {
+    if(array[index].length > maiorPalavra.length) {
+        maiorPalavra = array[index]
+    }
+}
+console.log(maiorPalavra)
+
+//Menor Palavra
+
+let menorPalavra = array[0];
+
+for (let index2 = 0; index2 < array.length; index2 +=1) {
+    if(array[index2].length < menorPalavra.length) {
+        menorPalavra = array[index2]
+    }
+}
+console.log(`A menor palavra é: ${menorPalavra}`)
