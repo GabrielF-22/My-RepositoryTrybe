1a47789152ff597d0fc4e6adf06936e0e84d2f8b
GabrielF-22 <gabriel.fernandesl@outlook.com>
2022-08-24 10:45:10 -0300

    Mais exercícios feitos

---
+++ M1/B4/Dia-4/script_2.js
@@ -0,0 +1,15 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+}
+console.log(info.recorrente)
+
+for (let index in info) {
+  console.log(index)
+}
+
+for (let index in info) {
+  console.log(info[index])
+}
---
+++ M1/B4/Dia-4/script_5.js
@@ -0,0 +1,10 @@
+let info2 = {
+  personagem: 'Margarida e Tio Patinhas',
+  origem: 'Pato Donald e Christmas on Bear Mountain, Dell s Four Color Comics #178',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas',
+  recorrente: 'Sim'
+}
+
+for (let index in info2) {
+  console.log(info2[index])
+}
